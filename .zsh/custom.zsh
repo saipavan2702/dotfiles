96bab0a3ba87279c70e6adf0867089fc873713a9
saipavan2702 <msaipavan.69@gmail.com>
2025-05-25 21:51:07 +0530

    add-fzf-improv

--- .zsh/custom.zsh
+++ .zsh/custom.zsh
@@ -79,28 +79,31 @@ eval "$(starship init zsh)"
 eval "$(zoxide init --cmd cd zsh)"
 source <(fzf --zsh)
 
-  export FZF_CTRL_R_OPTS="
-  --color header:italic
-  --height=80%
-  --bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
-  --header 'CTRL-Y: Copy command into clipboard, CTRL-/: Toggle line wrapping, CTRL-R: Toggle sorting by relevance'
-  "
+export FZF_CTRL_R_OPTS="
+--style=full
+--color header:italic
+--height=80%
+--bind 'ctrl-y:execute-silent(echo -n {2..} | pbcopy)+abort'
+--header 'CTRL-Y: Copy command into clipboard, CTRL-/: Toggle line wrapping, CTRL-R: Toggle sorting by relevance'
+"
 
-  export FZF_CTRL_T_OPTS="
-  --walker-skip .git,node_modules,target
-  --preview 'bat -n --color=always {}'
-  --height=80%
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'
-  --header 'CTRL-/: Toggle preview window position'
-  "
+export FZF_CTRL_T_OPTS="
+--style=full
+--walker-skip .git,node_modules,target
+--preview 'bat -n --color=always {}'
+--height=80%
+--bind 'ctrl-/:change-preview-window(down|hidden|)'
+--header 'CTRL-/: Toggle preview window position'
+"
 
-  export FZF_ALT_C_OPTS="
-  --walker-skip .git,node_modules,target
-  --preview 'tree -C {}'
-  --height=80%
-  --bind 'ctrl-/:change-preview-window(down|hidden|)'
-  --header 'CTRL-/: Toggle preview window position'
-  "
+export FZF_ALT_C_OPTS="
+--walker-skip .git,node_modules,target
+--preview 'tree -C {}'
+--height=80%
+--bind 'ctrl-/:change-preview-window(down|hidden|)'
+--header 'CTRL-/: Toggle preview window position'
+"
+
 
 if [[ -n "$ZSH_DEBUGRC" ]]; then
   zprof
