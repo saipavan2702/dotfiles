9abd3bcd482e22225528cc3c30633c07ad20bc69
saipavan2702 <msaipavan.69@gmail.com>
2025-07-15 00:15:24 +0530

    config-chanes

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -51,3 +51,15 @@ alias lta3="_eza -lTag --level=3"
 
 
 alias -g G='| grep'
+
+
+#pomodoro
+alias work="timer 60m && terminal-notifier -message 'Pomodoro'\
+        -title 'Work Timer is up! Take a Break 😊'\
+        -appIcon '~/Pictures/pumpkin.png'\
+        -sound Crystal"
+        
+alias rest="timer 10m && terminal-notifier -message 'Pomodoro'\
+        -title 'Break is over! Get back to work 😬'\
+        -appIcon '~/Pictures/pumpkin.png'\
+        -sound Crystal"
