a1e0b4053062cc3dd603252c7629b1fee693d8b1
saipavan2702 <msaipavan.69@gmail.com>
2026-04-12 19:59:55 +0530

    adding-cell-ht-more

--- .config/nvim/lua/mmacha/plugins/colorscheme.lua
+++ .config/nvim/lua/mmacha/plugins/colorscheme.lua
@@ -305,4 +305,16 @@ return {
     },
     -- NOTE: verper
     { 'datsfilipe/vesper.nvim' },
+    -- NOTE: ember
+    {
+        "ember-theme/nvim",
+        name = "ember",
+        priority = 1000,
+        config = function()
+            require("ember").setup({
+                variant = "ember", -- "ember" | "ember-soft" | "ember-light"
+            })
+            vim.cmd("colorscheme ember")
+        end
+    },
 }
